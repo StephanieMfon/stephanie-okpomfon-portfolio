f1b1d1eb7a1a7af5e116dbf3bca6b7492e9f9b8f
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-25 11:20:03 +0100

    testing changes again

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -11,8 +11,8 @@ function Work({ position, company, location, type, duration }) {
       </div>
       <div className="flex justify-between items-center pt-2">
       {/* <div className="items-center pt-2"> */}
-        <div className="flex flex-col md:flex-row pr-4">
-          {/* <div className="flex items-center"> */}
+        {/* <div className="flex flex-col md:flex-row pr-4"> */}
+          <div className="flex items-center">
             <svg
               // className="dark:fill-light-heading fill-dark-heading min-w-fit"
               className="dark:fill-light-heading fill-dark-heading"
