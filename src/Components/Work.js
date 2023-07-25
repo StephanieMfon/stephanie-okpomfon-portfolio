d839a4a84b10c7639465fe27a7b3cfd2be24223a
Stephanie Okpo-Mfon <okpomfonstephanie@gmail.com>
2023-07-25 11:02:25 +0100

    testing changes

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -3,12 +3,12 @@ import React from "react";
 function Work({ position, company, location, type, duration }) {
   return (
     <article className="pt-8 border-b-2 border-dark-content pb-5 dark:border-light-content border-opacity-20 dark:border-opacity-20">
-      <div className="flex justify-between items-center">
+      {/* <div className="flex justify-between items-center">
         <h1 className="text-content md:text-lg lg:text-xl">{position}</h1>
         <div className="btn bg-greenbg text-green-text text-xs inline-block rounded-3xl px-3 py-1 min-w-fit">
           {type}
         </div>
-      </div>
+      </div> */}
       <div className="flex justify-between pt-2 items-center">
         <div className="flex flex-col md:flex-row">
           <div className="flex items-center pr-5">
